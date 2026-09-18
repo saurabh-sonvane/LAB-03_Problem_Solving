6aae77289d8b09c57a23e3f059859dadcbb8027d
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-18 16:04:40 +0530

    star form question

---
+++ Python/For_Loops/krishna/trangle_inverse_shape.py
@@ -0,0 +1,10 @@
+#trangle inverse shape star print
+
+for i in range(0,5):
+    for j in range(i+1):
+        print(" ",end="")
+    for k in range (i,5-1):
+        print("*",end="")
+    for k in range (i,5):
+        print("*",end="")
+    print()
---
+++ Python/For_Loops/krishna/trangle_shape_star.py
@@ -0,0 +1,8 @@
+#trangle shape star print
+
+for i in range(0,5):
+    for j in range(6-i):
+        print(" ",end="")
+    for k in range (2*i+1):
+        print("*",end="")
+    print()
